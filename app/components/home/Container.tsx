ed00d10fe02f6b6fbe5ec3cd78240194e81a0752
jisookoh <gosjioo0312@naver.com>
2025-05-31 00:26:56 +0900

    Feat: 전체 레이아웃 컴포넌트 생성 및 컬러 템플릿 추가

--- app/components/home/Container.tsx
+++ app/components/home/Container.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import styled from "@emotion/styled";
+import { Colors } from "@/app/styles";
+import { MaxWidthLayout } from "@/app/components/layout";
 
 export const HomeContainer = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <MaxWidthLayout>ss</MaxWidthLayout>
+    </S.Container>
+  );
 };
 
 namespace S {
   export const Container = styled.main`
-    background-color: #333;
-    color: #fff;
+    background-color: ${Colors.White};
+    color: ${Colors.DefaultTextColor};
   `;
 }
---
+++ app/styles/colors.ts
@@ -0,0 +1,6 @@
+export enum Colors {
+  White = "#ffffff",
+  Black = "#000000",
+  Black300 = "#333333",
+  DefaultTextColor = Black300,
+}
